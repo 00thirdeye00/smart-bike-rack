bfc1c3768204186047f22fbbffc448ca4ed49036
00thirdeye00 <00thirdeye00@gmail.com>
2021-05-10 07:25:38 +0200

    accel process and accel read function updated

---
+++ src/observer.c
@@ -0,0 +1,14 @@
+/*
+ * \author	Guru Mehar Rachaputi
+ *			Karl Wallentin
+ *
+ * /
+
+/*observer file
+ *
+ *	1.	read accelerometer sensor current state
+ *	2.	read distance sensor current state
+ *	3.	raise alarm
+ *	4.	send data to root
+ *
+ * /
